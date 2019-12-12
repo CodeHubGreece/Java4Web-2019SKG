2c4b07a6894c8263336ab5c739c592a79488dfcb
George Tzinos <geotzinos@gmail.com>
2019-12-12 17:52:41 +0200

    Added HTML validations

---
+++ front-end/examples/4-validations/scripts/user/home.js
@@ -0,0 +1,64 @@
+function logout() {
+    sessionStorage.removeItem(SESSION_STORAGE_LOGIN_TOKEN_NAME);
+    window.location.replace(ROOT_PATH + "/logout")
+}
+
+function populateDataTable(books) {
+    $("#books").append("<tbody>");
+    jQuery.each(books, function(i,book) {
+        $("#books").append("<tr id='bookRow" + book.id + "'><td>" + book.id + "</td><td>" + book.title + "</td></tr>");
+     });
+     $("#books").append("</tbody>");
+
+     $("#books tr").click(function() {
+        loadBook($(this).children("td").html());
+     });
+
+    $('#books').DataTable();
+}
+
+function loadBook(id) {
+    $.ajax({
+        url: ROOT_PATH + "/books/" + id
+    }).then(function(book) {
+       $("input[name=id]").val(book.id);
+       $("input[name=title]").val(book.title);
+       $("input[name=isbn]").val(book.isbn);
+    });
+};
+
+$(document).ready(function() {
+
+    $.ajax({
+        url: ROOT_PATH + "/books"
+    }).then(function(books) {
+        populateDataTable(books);
+    });
+
+    $("#saveButton").on('click', function(event){
+        event.preventDefault();
+        alert("To be done...");
+    });
+
+    $("#deleteButton").on('click', function(event){
+        event.preventDefault();
+        let bookId = $("input[name=id]").val();
+        $.ajax({
+            url: ROOT_PATH + "/books/" + bookId,
+            type : "DELETE",
+            dataType : 'json',
+            contentType: 'application/json',
+                success : function(result) {
+                    $("#bookRow" + bookId).remove();
+                    $("input[name=id]").val("");
+                    $("input[name=title]").val("");
+                    $("input[name=isbn]").val("");
+                },
+                error: function(xhr, resp, text) {
+                    console.log(xhr, resp, text);
+                    alert("Could not delete book!");
+                 }
+         })
+
+    });
+});
---
+++ front-end/examples/4-validations/scripts/validations/global-validations.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+
+    /**
+     * Native form validations
+     */
+    $("form:not([data-enable-validation]) input").on("keyup change focus", function () {
+        try {
+            const closestForm = $(this).closest("form");
+            if (!closestForm.length) {
+                console.warn("Submit button is undefined.");
+                return;
+            }
+            
+            // Enable / Disable submit button
+            const submitButton = $(closestForm).find("[type='button'],button");
+            if (!submitButton.length) {
+                console.warn("Submit button is undefined.");
+                return;
+            }
+            
+            const isFormValid = $(closestForm)[0].checkValidity();
+    
+            submitButton.attr("disabled", !isFormValid);
+
+            // Show / Hide error label
+            const errorLabel = $(this).closest(".field").find(".error-label");
+            if (!errorLabel.length) {
+                console.warn("Error label is undefined.");
+                return;
+            }
+
+            const isCurrentTargetValid = $(this).is(":valid");
+            if (!isCurrentTargetValid) {
+                $(errorLabel).show();
+            } else {
+                $(errorLabel).hide();
+            }
+        }catch(e) {
+            console.error(e);
+        }
+    });
+
+    /**
+     * Custom validations
+     */
+    $("form[data-enable-validation] input[data-validation][data-validation!='submit']").on("keyup change focus", function () {
+        try {
+            const value = $(this).val() || "";
+            const validation_type = $(this).attr("data-validation");
+
+            switch (validation_type) {
+                case "no_numbers":
+                    validate(this, value, /^[^0-9]+$/);
+                    break;
+                default:
+                    console.warn("Wrong validation name.", validation_type);
+                    break;
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    });
+
+
+    function validate(element, value, regex) {
+
+        // Test element value
+        const isValid = regex.test(value);
+
+        if (!isValid) {
+            if ($(element).hasClass("valid-data")) {
+                $(element).removeClass("valid-data");
+            }
+
+            if (!$(element).hasClass("invalid-data")) {
+                $(element).addClass("invalid-data");
+            }
+
+        }
+        else {
+            if ($(element).hasClass("invalid-data")) {
+                $(element).removeClass("invalid-data");
+            }
+
+            if (!$(element).hasClass("valid-data")) {
+                $(element).addClass("valid-data");
+            }
+
+        }
+
+        changeSubmitButtonStatus(element);
+    }
+
+    function changeSubmitButtonStatus(element) {
+        // Disable/ enable button
+
+        const closestForm = $(element).closest("form");
+        if (!closestForm.length) {
+            console.warn("Submit button is undefined.");
+            return;
+        }
+
+        const submitButton = $(closestForm).find("[data-validation='submit']");
+        if (!submitButton.length) {
+            console.warn("Submit button is undefined.");
+            return;
+        }
+        
+        const allElements = $(closestForm).find("[data-validation][data-validation!='submit']").length;
+        const validElements = $(closestForm).find(".valid-data").length;
+
+        submitButton.attr("disabled", allElements !== validElements);
+    }
+})
